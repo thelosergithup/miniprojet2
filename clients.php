185d00063d43fd4f9d46aaeee33910c2d7adec47
nekhui ndie donald <donaldnekui554@gmail.com>
2024-06-23 12:32:30 +0200

    ajout du reste des dossiers

---
+++ clients.php
@@ -0,0 +1,17 @@
+
+<?php 
+    $client=true;
+    include_once("header.php");
+?>
+
+<!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Clients</h1>
+        </div>
+      </div>
+    
+
+<?php
+    include_once("footer.php");
+?>
---
+++ commandes.php
@@ -0,0 +1,17 @@
+
+<?php 
+    $commande=true;
+    include_once("header.php");
+?>
+
+<!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Commandes</h1>
+        </div>
+      </div>
+    
+
+<?php
+    include_once("footer.php");
+?>
